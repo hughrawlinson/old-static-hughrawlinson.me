2d3b19640c2ac1ae89edcd91fee1d702b97c137b
hughrawlinson <hugh@codeoclock.net>
2015-02-10 12:18:02 +0000

    chaged linking behaviour in github recent script

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,13 @@
 $(function(){
-    console.log('got called');
     $.ajax({
         url:'https://api.github.com/users/hughrawlinson/repos?sort=pushed&direction=desc',
         success:function(data,status){
             if(status === "success"){
+                console.log(data[0]);
                 $('.gh-recent').show(400);
                 $('.gh-recent #repo-name').text(data[0].name);
-                $('.gh-recent #repo-name').attr('href',data[0].html_url);
+                var link = data[0].homepage!=null?data[0].homepage:data[0].html_url;
+                $('.gh-recent #repo-name').attr('href',link);
                 $('.gh-recent #repo-desc').text(data[0].description);
             }
         }
